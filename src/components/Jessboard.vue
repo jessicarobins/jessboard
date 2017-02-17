acd79ba5069cd309cc0e24081c5335ef686fd84e
Jess <jessrrobins@gmail.com>
2017-02-17 01:18:53 +0000

    a list of sounds

---
+++ src/components/Jessboard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div v-for="sound in sounds">
+      {{ sound.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'jessboard',
+  data () {
+    return {
+      sounds: [{
+          name: "tony"
+        }, {
+          name: "it's lunchtime"
+        }, {
+          name: "hey brian"
+        }, {
+          name: "oh nooo"
+        }, {
+          name: "laters"
+        }, {
+          name: "get outta here bear"
+        },{
+          name: "hey you guys"
+        }, {
+          name: "hi nathalie"
+        }, {
+          name: "giggles"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
